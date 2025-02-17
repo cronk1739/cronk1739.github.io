1caaeeaacc77c8df938e62c8a83efd87f51d7fad
Sebastian <sc0474254@gmail.com>
2025-02-17 16:11:07 +0000

    ts frickin tuff

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -48,11 +48,10 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3])
-            physikz.updatePosition(circles[4]);;
+            for(var p = 0; p < circles.length; p++){
+                physikz.updatePosition(circles[p])
+                game.checkCirclePosition(circles[p])
+            }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
             game.checkCirclePosition(circles[0]);
