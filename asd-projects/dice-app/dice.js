59c82b18c599f1afd0e18a606749cbc5a63e8822
Sebastian <sc0474254@gmail.com>
2025-09-02 15:21:55 +0000

    hgefsYGFjk,ETVJuk, sehjzcm evnb

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
   // Your code goes here
-
-});
+$("<div>")
+  .css("height", 15)
+  .css("width", 15)
+  .css("background-color", "black")
+  .css("position", "absolute")
+  .css("top", 50)
+  .css("left", 50)
+  .appendTo("#die");
+}
+ function rollDie(){
+  console.log("clicked")
+}
+);
