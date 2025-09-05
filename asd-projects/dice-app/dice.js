112dbfdb02bd1d7e7266ac53e3698ddd9cab5654
Sebastian <sc0474254@gmail.com>
2025-09-05 11:41:29 +0000

    meowmoenwahwyhrhrhaysyshrbtjejahzjdnyntbagxtjtnybayHDOOGTNWGTdjt

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,15 +1,67 @@
 $(document).ready(function () {
   // Your code goes here
 $("<div>")
-  .css("height", 15)
-  .css("width", 15)
-  .css("background-color", "black")
-  .css("position", "absolute")
-  .css("top", 50)
-  .css("left", 50)
-  .appendTo("#die");
+.css("height", 15)
+.css("width", 15)
+.css("background-color", "white")
+.css("position", "absolute")
+.css("top", 42)
+.css ("left", 42)
+.appendTo("#die")
+$("#die").on("click", rollDie);
+
+
+function makeDot(top, left, die){
+$("<div»")
+.css("height", 15)
+.css("width", 15)
+.css ("top", top)
+.css("left", left)
+.css ("background-color", "white")
+.css("position", "absolute")
+.appendTo (die);
 }
- function rollDie(){
-  console.log("clicked")
+
+function rollDie() {
+$("#die"). empty()
+var randomNum = Math. ceil (Math. random() * 6);
+alert("You rolled a " + randomNum);
+if (randomNum = 1) { makeDot (42, 42, "#die"); // middle middle
 }
-);
+
+else if (randomNum === 2) {
+ makeDot (10, 10, "#die") // top left 
+ makeDot (75, 75, "#die") // bottom right
+}
+else if (randomNum === 3) {
+makeDot (10, 10,"#die"); // top left
+makeDot (75, 75, "#die") // bottom right
+makeDot (42, 42, "#die") // middle middle
+}
+
+else if (randomNum === 4) { 
+makeDot (75, 75, "#die"); // bottom right 
+makeDot (10, 10,"#die") // top left
+makeDot (10, 75, "#die") // top right
+makeDot (75, 10, "#die") // bottom left
+}
+
+else if (randomNum === 5) {
+makeDot (42, 42, "#die"); // middle middle
+makeDot (75, 75, "#die") // bottom right
+makeDot (10, 10, "#die") // top left
+makeDot (10, 75, "#die") // top right
+makeDot (75, 10, "#die") // bottom left
+}
+
+else if (randomNum = 6){
+makeDot (75, 75, "#die") // bottom right
+makeDot (10, 10, "#die") // top left
+makeDot (10, 75, "#die") // top rigit
+makeDot (75, 10, "#die") // bottom left
+makeDot (42, 10, "#die") // middle left
+makeDot (42, 75, "#die") // bottom left
+  }
+ } 
+});
+//* Elijah Was Here! *//
