1e962b5071ca7c4bf962d2127f9d60c4da11bbd4
Sebastian <sc0474254@gmail.com>
2025-10-01 19:07:44 +0000

    derp derp derp depr derp dper derp derp ederpodo wajkojtjha4wgiuhbiougfwpiugh

---
+++ BIG-PROJECT/begining.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to the beginning of your C++ program!" << std::endl;
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world";
+    return 0;
+}
+#include <iostream> 
+
+int main(){
+std::cout << "Hello World";
+return 0;
+}
+#include <iostream>
+int main(){
+cout << "MEOW!";
+}
